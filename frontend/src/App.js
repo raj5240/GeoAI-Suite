d1cb56b7ac925673b3111c46f70e4c6de89134d8
raj5240 <raj5240kiran@gmail.com>
2025-09-20 19:50:31 +0530

    app.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MapViewer from "./components/MapViewer";
+
+function App() {
+  return (
+    <div style={{height: "100vh"}}>
+      <MapViewer />
+    </div>
+  );
+}
+
+export default App;
